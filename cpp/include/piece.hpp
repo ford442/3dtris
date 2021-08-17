b625f882a9f00e2d1d2204d0ea1b9ee52715d82e
Noah Wolfe Cohn <noahc42@gmail.com>
2021-08-16 19:46:07 -0700

    clion

--- cpp/include/piece.hpp
+++ cpp/include/piece.hpp
@@ -1,48 +1,34 @@
 #ifndef __PIECE_HPP__
 #define __PIECE_HPP__
-
 #include "api.hpp"
 #include <vector>
-
-struct Rotation {
-    Axis axis;
-    RotationDirection direction;
+struct Rotation{
+Axis axis;
+RotationDirection direction;
 };
-
-struct Piece {
+struct Piece{
 private:
-    Pos3d center;
-    std::vector<Block> blocks;
-
+Pos3d center;
+std::vector <Block> blocks;
 public:
-    Piece(std::vector<Block> blocks_)
-    : Piece(Pos3d {0,0,0}, blocks_) {}
-
-    Piece(Pos3d center_, std::vector<Block> blocks_)
-    : center(center_), blocks(blocks_) {}
-
-    Piece(const Piece& other) = default;
-    std::vector<Block> getBlocks() const;
-
-    Piece translated(Pos3d) const;
-    Piece rotated(Rotation) const;
-
-    // would not need access to private data
-    Piece translatedBeyond(Axis axis, int limit, int direction) const;
-    int getExtent(Axis axis, int direction) const;
+Piece(std::vector <Block> blocks_) : Piece(Pos3d{0,0,0},blocks_){}
+Piece(Pos3d center_,std::vector <Block> blocks_) : center(center_),blocks(blocks_){}
+Piece(const Piece &other) = default;
+std::vector <Block> getBlocks() const;
+Piece translated(Pos3d) const;
+Piece rotated(Rotation) const;
+// would not need access to private data
+Piece translatedBeyond(Axis axis,int limit,int direction) const;
+int getExtent(Axis axis,int direction) const;
 };
-
-namespace block_methods {
-    Block rotate(const Block&, Rotation);
-    Block translate(const Block&, Pos3d);
+namespace block_methods{
+Block rotate(const Block &,Rotation);
+Block translate(const Block &,Pos3d);
 }
-
-namespace pos_methods {
-    inline Pos3d sum(Pos3d a, Pos3d b) {
-        return Pos3d { a.x + b.x, a.y + b.y, a.z + b.z };
-    }
-
-    int getElement(const Pos3d &pos, Axis ax);
+namespace pos_methods{
+inline Pos3d sum(Pos3d a,Pos3d b){
+return Pos3d{a.x+b.x,a.y+b.y,a.z+b.z};
 }
-
+int getElement(const Pos3d &pos,Axis ax);
+}
 #endif
